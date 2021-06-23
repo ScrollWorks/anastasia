723fef20e515e1a6be42a834cfb9444b4b5c3108
Anastasia <a.potapovafilina@gmail.com>
2021-06-23 14:55:27 +0200

    ejercicios Rucencito

--- cadenaMasLarga.js
+++ cadenaMasLarga.js
@@ -1,6 +1,29 @@
-function cadenaMasLarga(/*anyade los parametros que necesites*/) {
+let arr = ["hola", "anastasia", "como", "estas"];
+
+function cadenaMasLarga(arr) {
   // escribe aqui el codigo
-  
+  let longestWord = '';
+
+  for(let i=0; i<arr.length; i++){
+    if(arr[i].length > longestWord.length){
+      longestWord = arr[i];
+    }
+  }
+  // arr.forEach(ltr => {
+  //   if(arr[ltr] > longestWord.length){
+  //     longestWord = arr[ltr];
+  //   }
+  // });
+  return longestWord;
 }
 
-module.exports = cadenaMasLarga;
+// function cadenaMasLarga(arr) {
+//   // escribe aqui el codigo
+//   let longestWord = arr.reduce((a,b) => a.length > b.length ? a : b, '');
+//   return longestWord;
+// }
+
+console.log(cadenaMasLarga(arr));
+
+
+//module.exports = cadenaMasLarga;
--- sumaParesImpares.js
+++ sumaParesImpares.js
@@ -1,6 +1,28 @@
-function sumaParesImpares(/*anyade los parametros que necesites*/) {
-  // escribe aqui el codigo
+//let arrNum = [1,2,3,4,5,6,7,8,9];
 
+function sumaParesImpares(arrNum) {
+  // escribe aqui el codigo
+  const obj = {};
+ obj.pares = arrNum.reduce((acc, cur) => acc + (cur % 2 === 0 ? cur : 0), 0);
+ obj.impares = arrNum.reduce((acc, cur) => acc + (cur % 2 !== 0 ? cur : 0), 0);
+ return obj;
 }
 
+
+
+
+
 module.exports = sumaParesImpares;
+
+// function sumaParesImpares(arrNum) {
+//   // escribe aqui el codigo
+//   const obj = {};
+//   for(i=0; i<arrNum.length; i++){
+//     if(arrNum[i] % 2 === 0){
+//       obj.even += arrNum[i];
+//     }else{
+//       obj.odd += arrNum[i];
+//     }
+//   }
+//   return obj;
+// }
